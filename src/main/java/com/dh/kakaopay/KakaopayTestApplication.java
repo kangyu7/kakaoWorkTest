7b2ab1d3ad44cd03fc3bbd8410f93685b2b385ff
Dong Ho Kang <kangyu7@gmail.com>
2020-07-09 09:00:49 +0900

    init

---
+++ src/main/java/com/dh/kakaopay/KakaopayTestApplication.java
@@ -0,0 +1,13 @@
+package com.dh.kakaopay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaopayTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaopayTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dh/kakaopay/KakaopayTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dh.kakaopay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaopayTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
